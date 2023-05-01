eabe20718e4fcdf48f30c7340d34fbb863fe8903
Paula Zaidel <paulazaaidel@gmail.com>
2023-05-01 10:09:06 -0300

    configure repositories

--- BarbecueManager.Application.API/Program.cs
+++ BarbecueManager.Application.API/Program.cs
@@ -1,4 +1,6 @@
+using BarbecueManager.Domain.Interfaces;
 using BarbecueManager.Infra.Data;
+using BarbecueManager.Infra.Data.Repositories;
 
 var builder = WebApplication.CreateBuilder(args);
 var connectionString = builder.Configuration.GetConnectionString("SqliteConnectionString");
@@ -10,6 +12,10 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
+builder.Services.AddScoped<IPersonRepository, PersonRepository>();
+builder.Services.AddScoped<IBarbecueRepository, BarbecueRepository>();
+
 
 var app = builder.Build();
 
